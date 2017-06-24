e3c587264a6e96fbe07adb10a0bb92b38eb24e30
Rohan Jacob-Rao <rohanjr@gmail.com>
2017-06-24 16:35:40 +1000

    Remove commented out code from main

--- infer/src/go/main.ml
+++ infer/src/go/main.ml
@@ -9,7 +9,6 @@
  * Shawn Otis 
  *)
 
-(*exception UsageError of string*)
 open Printf
 open Lexing
 open Pretty
@@ -44,11 +43,6 @@ and process_args args =
       else Some arg
 
 let () = try
-  (*if Array.length Sys.argv < 2 then ()
-    raise (UsageError ("Missing command line argument. " ^
-                       "Give base name for output files."))     
-  else
-    let basename = Sys.argv.(1) in*)
       let filenameOpt = process_args (List.tl (Array.to_list Sys.argv)) in      
       if !help then fprintf stdout 
       "Name: Group15 GoLite -> C Compiler\nSynopsis: ./main.native [option]... [file]\nDescription: Compile GoLite code into C11.\n-dumpsymtab : Dump the symbol table while compiling.\n-pptype : Pretty print with types.\n-nopretty : Suppress the creation of a pretty printed file.\n-nocompile : Suppress the compilation (for debug).\n--help, -h : Help.\n--version, -v : Compiler version.\n" 
@@ -75,13 +69,7 @@ let () = try
       (if !pptype then let oc' = open_out (String.sub filename 0 (String.rindex filename '.') ^ ".pptype.go") in fprintf oc' "%s" (Pretty.pretty weeded_two));
       (if !compile then let oc' = open_out (String.sub filename 0 (String.rindex filename '.') ^ ".c") in fprintf oc' "%s" (Codegen.gen_prog weeded_two));
       flush stdout;
-      (*Typecheck.type_of_prog p;
-      output_string (open_out (basename ^ ".symbol.txt"))
-                    (Typecheck.string_of_symtable Typecheck.symtable);
-      output_string (open_out (basename ^ ".pretty.min")) (Pretty.pretty_prog p);
-      output_string (open_out (basename ^ ".c")) (Codegen.code_of_prog p)*)
 with
-  (*| UsageError msg -> print_string ("Usage error. " ^ msg ^ "\n")*)
   | Lexer.LexerError msg -> fprintf stderr "Lexer error: %s\n" msg
   | Parser.Error -> fprintf stderr "Syntax error.\n"
   | E.ParsingError (s, p) -> fprintf stderr "%s\n" (string_of_pos p ^ ": error: " ^ s)
@@ -90,6 +78,3 @@ with
   | Check.TypeError s -> fprintf stderr "Type error: %s\n" s
   | Check.DeclError s -> fprintf stderr "Decl error: %s\n" s
   | Check.InternalError s -> fprintf stderr "Internal error: %s\n" s
-  (*| Typecheck.TypeError msg -> print_string ("Type error. " ^ msg ^ "\n")
-  | Typecheck.DeclError msg -> print_string ("Declaration error. " ^ msg ^ "\n")
-  | Codegen.InternalError msg -> print_string ("Internal error. " ^ msg ^ "\n")*)
